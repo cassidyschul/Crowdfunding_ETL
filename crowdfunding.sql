24526d944e02142fe0301fd0ec897674dfde09ba
AbelDumecha <161157593+AbelDumecha@users.noreply.github.com>
2024-05-23 12:44:41 -0400

    Add files via upload

---
+++ crowdfunding.sql
@@ -0,0 +1,55 @@
+-- Create contact, category, subcategory and campaign tables
+CREATE TABLE contacts (
+    contact_id INT   NOT NULL,
+    first_name VARCHAR(255)   NOT NULL,
+    last_name VARCHAR(255)   NOT NULL,
+    email VARCHAR(255)   NOT NULL,
+    CONSTRAINT pk_contacts PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE category (
+    category_id VARCHAR(30)   NOT NULL,
+    category VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(30)   NOT NULL,
+    subcategory VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_subcategory PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE campaign (
+    cf_id INT   NOT NULL,
+    contact_id INT   NOT NULL,
+    company_name VARCHAR(255)   NOT NULL,
+    descriptipon VARCHAR(255)   NOT NULL,
+    goal FLOAT   NOT NULL,
+    pledged FLOAT   NOT NULL,
+    outcome VARCHAR(30)   NOT NULL,
+    backers_count INT   NOT NULL,
+    country VARCHAR(30)   NOT NULL,
+    currency VARCHAR(30)   NOT NULL,
+    launch_date Date   NOT NULL,
+    end_date Date   NOT NULL,
+    category_id VARCHAR(30)   NOT NULL,
+    subcategory_id VARCHAR(30)   NOT NULL,
+	PRIMARY KEY(cf_id),
+	CONSTRAINT fk_contact FOREIGN KEY (contact_id) 
+		REFERENCES contacts (contact_id),
+	CONSTRAINT fk_category FOREIGN KEY (category_id) 
+		REFERENCES category (category_id),
+	CONSTRAINT fk_subcategory FOREIGN KEY (subcategory_id) 
+		REFERENCES subcategory (subcategory_id)
+);
+
+-- Verify the tables creation, import each CSV file into its corresponding SQL table and again verify that each table has the correct data
+select * from contacts;
+
+select * from category;
+
+select * from subcategory;
+
+select * from campaign;
+
+---
